0e06b83d50fac3991d7ca9dd6e9ea7e04837db0c
Evan Nagle <evan@mantle.co>
2018-10-04 16:28:08 -1000

    Starting to setup the ./scripts libs

---
+++ .scripts/make-book.el
@@ -0,0 +1,6 @@
+(defun omu/make-book (title author publisher year pages file)
+  "Create a new book and add it to the omu library."
+  (interactive "sTitle: \nsAuthor: \nsPublisher: \nsYear: \nsPages: \nsPDF: ")
+  (message "Sorry. This isn't really working yet."))
+
+(provide 'omu-make-book)
